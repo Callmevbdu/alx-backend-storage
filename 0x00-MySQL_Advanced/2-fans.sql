3adc136eded04c659eb42e29bdde77cfa3b9aac2
Abdellatif <abdelt.2012@gmail.com>
2024-06-13 22:12:41 +0100

    Done.

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,11 @@
+-- This SQL script ranks country origins of bands ordered by the number of fans
+
+SELECT 
+    origin, 
+    COUNT(fans) AS nb_fans
+FROM 
+    metal_bands
+GROUP BY 
+    origin
+ORDER BY 
+    nb_fans DESC;
