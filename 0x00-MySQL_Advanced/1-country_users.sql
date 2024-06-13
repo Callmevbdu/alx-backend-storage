0c5ad1d40ddac0e80bd13941bf9a2aa5ab48b298
Abdellatif <abdelt.2012@gmail.com>
2024-06-13 22:10:55 +0100

    Done.

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,15 @@
+-- This SQL script creates a table named 'users' if it doesn't already exist.
+
+CREATE TABLE IF NOT EXISTS users (
+    -- 'id' is an integer that auto-increments.
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    
+    -- 'email' is a string of up to 255 characters.
+    email VARCHAR(255) NOT NULL UNIQUE,
+    
+    -- 'name' is a string of up to 255 characters.
+    name VARCHAR(255),
+    
+    -- 'country' is an enumeration of countries ('US', 'CO', 'TN').
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
