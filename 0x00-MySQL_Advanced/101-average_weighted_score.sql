9c229d78c41a69570646d03cd6f8c02b48cb8e46
Abdellatif <abdelt.2012@gmail.com>
2024-06-14 02:57:51 +0100

    Done.

---
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -0,0 +1,28 @@
+-- a SQL script that creates a stored procedure
+-- ComputeAverageWeightedScoreForUsers that computes and store the average
+-- weighted score for all students.
+DELIMITER $$
+
+CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
+BEGIN
+    DECLARE done INT DEFAULT FALSE;
+    DECLARE cur CURSOR FOR SELECT id FROM users;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+    DECLARE @user_id INT;
+
+    OPEN cur;
+
+    read_loop: LOOP
+        FETCH cur INTO @user_id;
+
+        IF done THEN
+            LEAVE read_loop;
+        END IF;
+
+        CALL ComputeAverageWeightedScoreForUser(@user_id);
+    END LOOP;
+
+    CLOSE cur;
+END$$
+
+DELIMITER ;
