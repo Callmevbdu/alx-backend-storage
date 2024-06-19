aaffdec85e32e4b38b64f954eeb85954a31b0fe1
Abdellatif <abdelt.2012@gmail.com>
2024-06-20 00:25:19 +0100

    Done.

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,13 +6,29 @@ Redis server.
 import redis
 import uuid
 from typing import Union, Callable, Optional
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """
+    Decorator that counts how many times methods of the Cache class are called.
+    """
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+
+    return wrapper
 
 
 class Cache:
     """
-    The Cache class encapsulates methods for storing and retrieving data using
-    Redis.
+    The Cache class encapsulates methods for storing and retrieving data
+    using Redis.
     """
+
     def __init__(self):
         """
         Initialize the Cache instance with a Redis client connection and flush
@@ -21,6 +37,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Store the input data in Redis using a random key and return the key.
@@ -29,25 +46,9 @@ class Cache:
         self._redis.set(random_key, data)
         return random_key
 
-    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:  # noqa
+    def get(self, key: str) -> Union[str, bytes]:
         """
-        Retrieve the stored data by key and convert it using the provided
-        Callable.
+        Retrieve the stored data by key from Redis.
         """
         value = self._redis.get(key)
-        if fn is not None and value is not None:
-            return fn(value)
-        return value
-
-    def get_str(self, key: str) -> Optional[str]:
-        """
-        Retrieve a string value by key from Redis.
-        """
-        return self.get(key, fn=lambda x: x.decode('utf-8'))
-
-    def get_int(self, key: str) -> Optional[int]:
-        """
-        Retrieve an integer value by key from Redis.
-        """
-        value = self.get(key)
-        return int(value) if value is not None else None
+        return value if value is not None else None
